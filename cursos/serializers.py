89dae2c1ea79db7b175b9e7ea7e522976702a717
Murilo Scorsato <scorsato86murilo@gmail.com>
2025-03-16 14:12:38 -0300

    view criada com serializers

--- cursos/serializers.py
+++ cursos/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 from .models import Curso, Avaliacao
 
-class AvaliacaoSerializer(serializers.ModelSerializer)
+class AvaliacaoSerializer(serializers.ModelSerializer):
     class Meta:
         extra_kwargs = {
             'email': {'write_only': True}
--- cursos/views.py
+++ cursos/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Curso, Avaliacao
+from .serializers import CursoSerializer, AvaliacaoSerializer
 
-# Create your views here.
+class CursoAPIView(APIView):
+    def get(self, request):
+        cursos = Curso.objects.all()
+        serializer = CursoSerializer(cursos, many=True)
+        return Response(serializer.data)
+
+
+class AvaliacaoAPIView(APIView):
+    def get(self, request):
+        avaliacaos = Avaliacao.objects.all()
+        serializer = AvaliacaoSerializer(avaliacaos, many=True)
+        return Response(serializer.data)
