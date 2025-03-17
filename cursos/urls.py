bf7962ad03b58c8f9f49c7e8e4acf126594db81a
Murilo Scorsato <scorsato86murilo@gmail.com>
2025-03-17 10:45:40 -0300

    atualizando o arquivo view

--- cursos/urls.py
+++ cursos/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from .views import CursoAPIView, AvaliacaoAPIView
+from .views import CursosAPIView, AvaliacoesAPIView, CursoAPIView, AvaliacaoAPIView
 
 urlpatterns = [
-    path('cursos/', CursoAPIView.as_view(), name='cursos'),
-    path('avaliacao/', AvaliacaoAPIView.as_view(), name='avaliacao'),
+    path('cursos/', CursosAPIView.as_view(), name='cursos'),
+    path('cursos/<int:pk>', CursoAPIView.as_view(), name='curso'),
+
+    path('avaliacoes/', AvaliacoesAPIView.as_view(), name='avaliacoes'),
+    path('avaliacoes/<int:pk>', AvaliacaoAPIView.as_view(), name='avaliacao'),
+
 ]
